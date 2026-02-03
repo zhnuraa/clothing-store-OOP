87d6d152d26d6aa06f4a165ec382b64543f2a678
zhnuraa <250822@astanait.edu.kz>
2026-02-03 10:31:55 +0500

    Add DatabaseConnection class for PostgreSQL connection

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -1,1 +1,17 @@
+package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/clothing_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "ТВОЙ_ПАРОЛЬ";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
